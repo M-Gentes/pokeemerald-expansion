b48f13bc5353218b5509ca5ea082371e1637e9a2
M-Gentes <91728744+M-Gentes@users.noreply.github.com>
2023-02-24 19:14:29 +0100

    update trainers

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -2866,7 +2866,7 @@ const struct Trainer gTrainers[] = {
             .items = {},
             .doubleBattle = FALSE,
             .aiFlags = AI_FLAG_CHECK_BAD_MOVE,
-            .party = NO_ITEM_DEFAULT_MOVES(sParty_Cameron1),
+            .party = ITEM_DEFAULT_MOVES(sParty_Cameron1),
         },
 
     [TRAINER_CAMERON_2] =
