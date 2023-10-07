436eafd4a38616b8f9b747d9a84f27ce3ce660f2
M-Gentes <manuel.gentes@gmail.com>
2023-10-07 10:40:04 +0200

    fix trainer moveset

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -286,7 +286,7 @@ const struct Trainer gTrainers[] = {
             .items = {},
             .doubleBattle = FALSE,
             .aiFlags = AI_FLAG_CHECK_BAD_MOVE,
-            .party = NO_ITEM_DEFAULT_MOVES(sParty_GruntMtPyre1),
+            .party = NO_ITEM_CUSTOM_MOVES(sParty_GruntMtPyre1),
         },
 
     [TRAINER_GRUNT_MT_PYRE_2] =
