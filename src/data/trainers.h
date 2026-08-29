4acae1f6b8e9c61ce9212ade3074adb3c6dddeef
M-Gentes <91728744+M-Gentes@users.noreply.github.com>
2026-08-29 11:53:23 +0200

    fix dauni

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -274,7 +274,7 @@ const struct Trainer gTrainers[] = {
             .items = {},
             .doubleBattle = FALSE,
             .aiFlags = AI_FLAG_CHECK_BAD_MOVE,
-            .party = NO_ITEM_DEFAULT_MOVES(sParty_GruntSpaceCenter1),
+            .party = ITEM_DEFAULT_MOVES(sParty_GruntSpaceCenter1),
         },
 
     [TRAINER_GRUNT_MT_PYRE_1] =
